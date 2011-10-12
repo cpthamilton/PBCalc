4fdada10bc0d38902842a0d45c319bedfe6999d6
Abraham Pernicka <abepern@gmail.com>
2011-10-11 23:01:27 -0400

    Layout part 1

--- PBCalc/MainWindow.xaml.cs
+++ PBCalc/MainWindow.xaml.cs
@@ -19,9 +19,57 @@ namespace PBCalc
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int PointsRemaining;
+        private int StartingPointValue;
+        private enum Stats
+        {
+            Str = 0,
+            Con = 1,
+            Dex = 2,
+            Int = 3,
+            Wis = 4,
+            Cha = 5
+        };
+        private int[] CurrentStats;
+
         public MainWindow()
         {
             InitializeComponent();
+            comboBox1.SelectedIndex = 3;
+            resetStats(pointsFromCombo());
+        }
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            resetStats(pointsFromCombo());
+        }
+
+        private int pointsFromCombo()
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    return 15;
+                case 1:
+                    return 20;
+                case 2:
+                    return 22;
+                case 3:
+                    return 25;
+                case 4:
+                    return 28;
+                case 5:
+                    return 32;
+            }
+            return 25;
+        }
+        private void resetStats(int val)
+        {
+            StartingPointValue = val;
+            PointsRemaining = StartingPointValue;
+            CurrentStats = new int[6] { 8, 8, 8, 8, 8, 8 };
+
+            currentPoints.Text = PointsRemaining.ToString();
         }
     }
 }
